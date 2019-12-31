0a840c0f8a00ed802dacb0cff22dc7ac94e850e8
Huan LI <zixia@zixia.net>
2019-12-31 18:47:13 +0800

    test oss bot

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,52 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import {
+  OnCallback
+} from 'probot/lib/application'
+import Webhooks from '@octokit/webhooks'
+
+import FileBox from 'file-box'
+
+let n = 0
+
+const commentIssue: OnCallback<Webhooks.WebhookPayloadIssueComment> = async (context) => {
+  // const issue = context .issue()
+  const repoName = context.payload.repository.name
+  const issueNumber = context.payload.issue.number
+  const issueTitle = context.payload.issue.title
+  const commentBody = context.payload.comment.body
+  const htmlUrl = context.payload.comment.html_url
+  const avatarUrl = context.payload.comment.user.avatar_url
+
+  const title = [
+    repoName,
+    issueNumber,
+    issueTitle.slice(0, Math.max(issueTitle.length, 30)),
+  ].join(' ')
+  const url = htmlUrl
+  const description = commentBody.slice(0, Math.max(commentBody.length, 70))
+  const thumbnailUrl = avatarUrl
+
+  const webhook = [
+    'https://oss-bot.kaiyuanshe.cn/webhook/',
+      [
+        `url=${encodeURI(url)}`,
+        `description=${encodeURI(description)}`,
+        `thumbnailUrl=${encodeURI(thumbnailUrl)}`,
+        `title=${encodeURI(title)}`,
+      ].join('&'),
+    ].join('?')
+
+  const result = (await FileBox.fromUrl(webhook).toBuffer()).toString()
+  console.info('url:', webhook)
+  console.info('result:', result)
+
+  // const issueComment = context.issue({ body: `Thanks for comment this issue! ${n++}` })
+  // await context.github.issues.createComment(issueComment)
+  // console.info(context)
+}
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-    // console.info(context)
-  })
+  app.on('issue_comment.created', commentIssue)
 
   // For more information on building apps:
   // https://probot.github.io/docs/
